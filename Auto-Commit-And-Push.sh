2db886bc601727f8dcde9507bc7ef4bb8ad6620c
unknown <kmumar1618@gmail.com>
2026-06-13 12:48:08 +0500

    unfinished

--- Auto-Commit-And-Push.sh
+++ Auto-Commit-And-Push.sh
@@ -8,18 +8,21 @@ if [ -z "$(git status --porcelain)" ]; then
         exit 0
     fi
 else
+	#..........checking branch name
+
     branch=$(git branch --show-current)
-    echo "$branch"
+    echo "$branch is your branch name"
+    # ..........changing brach, committing and pushing
     read -p "If You Want To Push In This Branch y/n: " branchask 
     if [ "$branchask" == "N"] || [ "$branchask" == "n" ]; then
-       read -p "Please Enter Your Correct Branch Name: " branchname
+       read -p "Please Enter Your Correct Branch Name: " branch
        read -p "Do You Want To Send All Files y/n: " msg
        if [ "$msg"  == "Y" ] || [ "$msg" == "y" ]; then
           read -p "Enter Commit Name: " name
           git add . 
 
           git commit -m "$name"
-	  git push origin "$branchname"
+	  git push origin "$branch"
  	  echo "Pushed to remote repository"
 	  git status
        else
@@ -28,7 +31,7 @@ else
           git add "$file"
           git commit -m "$name2"
           
-	  git push origin "$branchname"
+	  git push origin "$branch"
  	  echo "Pushed to remote repository"
 	  git status
     else        
